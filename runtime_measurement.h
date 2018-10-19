d90dc43556ff109bd38a9e2620028d0c2b4114f2
Balint Telkes <balintt21@gmail.com>
2018-10-19 13:56:53 +0200

    Update runtime_measurement.h

--- runtime_measurement.h
+++ runtime_measurement.h
@@ -6,6 +6,7 @@
 #include <utility>
 #include <unistd.h>
 #include <functional>
+#include <chrono>
 
 class RuntimeMeasurement
 {
@@ -14,12 +15,15 @@ public:
     {
         int64_t user_time_clktck;
         int64_t system_time_clktck;
+        int64_t duration_ns;
         int64_t clocks_per_sec;
 
+        double durationToSec() const { return static_cast<double>(duration_ns) / 1000000000.0; }
         double userTimeToSec() const { return (user_time_clktck / static_cast<double>(clocks_per_sec)); }
         double systemTimeToSec() const { return (system_time_clktck / static_cast<double>(clocks_per_sec)); }
     };
 protected:
+    int64_t                                 mStartTime;
     struct tms                              mStartTimes;
     struct tms                              mCurrentTimes;
     std::function<void (const Result&)>  mResultCallback;
@@ -29,7 +33,9 @@ public:
     inline Result measure()
     {
         times(&mCurrentTimes);
-        Result result {  (mCurrentTimes.tms_utime - mStartTimes.tms_utime)
+        Result result {  std::chrono::time_point_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now())
+                                                                               .time_since_epoch().count() - mStartTime
+                       , (mCurrentTimes.tms_utime - mStartTimes.tms_utime)
                        , (mCurrentTimes.tms_stime - mStartTimes.tms_stime)
                        , sysconf(_SC_CLK_TCK) };
         if( mResultCallback ) { mResultCallback(result); }
@@ -38,6 +44,7 @@ public:
     
     RuntimeMeasurement(const std::function<void (const Result&)>& result_callback = nullptr) : mResultCallback(result_callback)
     {
+        mStartTime = std::chrono::time_point_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now()).time_since_epoch().count();
         times(&mStartTimes);
     }
     
