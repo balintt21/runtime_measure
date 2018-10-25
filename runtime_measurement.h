1cfcb7eeae7c2c9470bd5ffea26a873fd7179843
Balint Telkes <balintt21@gmail.com>
2018-10-25 10:34:26 +0200

    Update runtime_measurement.h

--- runtime_measurement.h
+++ runtime_measurement.h
@@ -2,7 +2,8 @@
 #define _RUNTIME_MEASUREMENT_H_
 
 #include <stdint.h>
-#include <sys/times.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #include <utility>
 #include <unistd.h>
 #include <functional>
@@ -13,31 +14,26 @@ class RuntimeMeasurement
 public:
     struct Result
     {
-        int64_t user_time_clktck;
-        int64_t system_time_clktck;
+        int64_t user_time_us;
+        int64_t system_time_us;
         int64_t duration_ns;
-        int64_t clocks_per_sec;
-
-        double durationToSec() const { return static_cast<double>(duration_ns) / 1000000000.0; }
-        double userTimeToSec() const { return (user_time_clktck / static_cast<double>(clocks_per_sec)); }
-        double systemTimeToSec() const { return (system_time_clktck / static_cast<double>(clocks_per_sec)); }
     };
 protected:
     int64_t                                 mStartTime;
-    struct tms                              mStartTimes;
-    struct tms                              mCurrentTimes;
+    struct rusage                           mStartTimes;
+    struct rusage                           mCurrentTimes;
     std::function<void (const Result&)>  mResultCallback;
 public:
-    static int64_t CLOCK_TICKS_PER_SEC() { return sysconf(_SC_CLK_TCK); }
-
+    int64_t timevalToUSec(struct timeval& t) { return static_cast<int64_t>(t.tv_sec * 1000000ll) + static_cast<int64_t>(t.tv_usec); }
+    
     inline Result measure()
     {
-        times(&mCurrentTimes);
-        Result result {  (mCurrentTimes.tms_utime - mStartTimes.tms_utime)
-                       , (mCurrentTimes.tms_stime - mStartTimes.tms_stime)
+        getrusage(RUSAGE_THREAD, &mCurrentTimes);
+        Result result {  (timevalToUSec(mCurrentTimes.ru_utime) - timevalToUSec(mStartTimes.ru_utime))
+                       , (timevalToUSec(mCurrentTimes.ru_stime) - timevalToUSec(mStartTimes.ru_stime))
                        , std::chrono::time_point_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now())
                                                                                .time_since_epoch().count() - mStartTime
-                       , sysconf(_SC_CLK_TCK) };
+                       };
         if( mResultCallback ) { mResultCallback(result); }
         return result;
     }
@@ -45,7 +41,7 @@ public:
     RuntimeMeasurement(const std::function<void (const Result&)>& result_callback = nullptr) : mResultCallback(result_callback)
     {
         mStartTime = std::chrono::time_point_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now()).time_since_epoch().count();
-        times(&mStartTimes);
+        getrusage(RUSAGE_THREAD, &mStartTimes);
     }
     
     ~RuntimeMeasurement()
